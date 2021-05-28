3d9761501b5db5bc44e4e7f819754bd8db0226ec
ryanp <ryanpanimation@gmail.com>
2021-05-28 00:10:42 -0500

    add netlify config to handle https redirect

--- app/src/App.js
+++ app/src/App.js
@@ -51,7 +51,7 @@ function App() {
 
   const asyncGet = async (params) => {
     const apiResponse = await axios.get('http://api.weatherstack.com/current', { params });
-    apiResponse.data ? setWeatherData(apiResponse.data) : setWeatherData(TEST_DATA);
+    apiResponse.data.error ? setWeatherData(TEST_DATA) : setWeatherData(apiResponse.data);
   };
 
   useEffect(
